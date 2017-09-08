ccdcbd64422f7a5582b88bd7fbbfe047c4ec78b0
Mark Fernando <markchrishantha13@gmail.com>
2017-09-08 17:11:12 +0530

    Add files via upload

---
+++ MessageReceiver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace MessageReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "YOUR_SERVICE_BUS_END_POINT";
+            var queueName = "queuemytest";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            client.OnMessage(message =>
+            {
+                Console.WriteLine(String.Format("Message body: {0}", message.GetBody<String>()));
+                Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+            });
+
+            Console.WriteLine("Press ENTER to exit program");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MessageSender.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace MessageSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isExits = false;
+            while (!isExits)
+            {
+                Console.WriteLine("Type the message and press enter to send. If you want to exit type 'exit'");
+                var message = Console.ReadLine();
+                if (!string.IsNullOrEmpty(message) && message != "exit")
+                    SendMessage(message);
+                isExits = message == "exit";
+            }
+        }
+
+        private static void SendMessage(string messageStr)
+        {
+            var connectionString = "YOUR_SERVICE_BUS_END_POINT";
+            var queueName = "queuemytest";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            var message = new BrokeredMessage(messageStr);
+
+            Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+
+            client.Send(message);
+
+            Console.WriteLine("Message successfully sent!");
+        }
+    }
+}
